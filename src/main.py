d6f7da7019a4f8ab7b255e15509b816964d2b085
Keith Kloos <agondol@yahoo.com>
2025-05-13 20:50:08 -0500

    Started Chapter 3

--- src/main.py
+++ src/main.py
@@ -20,4 +20,8 @@ def text_node_to_html_node(text_node):
         case TextType.IMAGE:
             return LeafNode("img", None, {"src": text_node.url, "alt": text_node.content})
 
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    split_nodes = old_nodes.content.split(delimiter)
+    for n in split_nodes
+
 main()
--- src/test_main.py
+++ src/test_main.py
@@ -1,6 +1,6 @@
 import unittest
 
-from main import text_node_to_html_node
+from main import text_node_to_html_node, split_nodes_delimiter
 from textnode import TextNode, TextType
 
 class TestMain(unittest.TestCase):
@@ -9,6 +9,11 @@ class TestMain(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
+
+    def test_split_nodes_delimiter(self):
+        node = TextNode("This is text with a 'code block' word", TextType.NORMAL)
+        new_nodes = split_nodes_delimiter([node], "'", TextType.CODE)
+        self.assertEqual(new_nodes, [TextNode("This is text with a ", TextType.NORMAL), TextNode("code block", TextType.CODE),TextNode(" word", TextType.NORMAL),])
 
 if(__name__ == "__main__"):
     unittest.main()
